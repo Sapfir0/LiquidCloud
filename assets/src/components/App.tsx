5638132cdc321326fb1f2e2c37e8721e9d2a30ec
Sapfir0 <sapfir999999@yandex.ru>
2021-02-05 13:43:42 +0300

    Добавил мусора

--- assets/src/components/App.tsx
+++ assets/src/components/App.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import { Index } from '../pages/Index/Index';
-import { ClientRoutes } from '../services/clientRouteContants';
-import { FileSystemChecker } from '../socket';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import {Index} from '../pages/Index/Index';
+import {ClientRoutes} from '../services/clientRouteContants';
+import {FileSystemChecker} from '../socket';
 import Header from './Header';
+
 const fs = new FileSystemChecker();
 
 const App: React.FC = () => (
-    <Router>
-        <Header />
-        <Switch></Switch>
-        <Route component={Index} path={ClientRoutes.Index} />
-        <Redirect to={ClientRoutes.Index} />
-    </Router>
+    <>
+        <Header/>
+        <Router>
+            <Switch>
+                <Route component={Index} path={ClientRoutes.Index}/>
+                <Redirect to={ClientRoutes.Index}/>
+            </Switch>
+        </Router>
+    </>
 );
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = (env, options) => ({
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'awesome-typescript-loader',
         }
       },
       {
