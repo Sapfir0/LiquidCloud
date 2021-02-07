3c2c1120d188dca0923d5a745fb84e2fe7e94a37
Sapfir0 <sapfir999999@yandex.ru>
2021-02-07 20:46:53 +0300

    fix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElCloud.MixProject do
     [
       app: :elCloud,
       version: "0.1.0",
-      elixir: "~> 1.11.0",
+      elixir: ">= 1.10.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
