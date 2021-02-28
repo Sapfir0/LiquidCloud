891ff56d8c76042c81b516dd70ed29adfdf50819
Sapfir0 <sapfir999999@yandex.ru>
2021-02-28 17:03:01 +0300

    file helper

--- test/ElCloud/file_storage/file_storage_test.exs
+++ test/ElCloud/file_storage/file_storage_test.exs
@@ -1,6 +1,7 @@
 defmodule ElCloud.FileStorageTest do
   use ElCloud.DataCase
   alias ElCloud.ListHelper
+  alias ElCloud.PathHelper
 
   def list_files_by_path(list, path) do
     ListHelper.recursive_find(list, :path, path)
@@ -12,32 +13,43 @@ defmodule ElCloud.FileStorageTest do
 
   describe "fileStorage" do
     alias ElCloud.FileStorage
-    @file_attrs %{folder: "./data", filepath: Path.join("./data", "/test.txt")}
+
+    @file_attrs %{folder: "./data", filepath: Path.join("./data", "test.txt")}
+    @inner_file_attrs %{folder: "./data/test_folder",  filepath: PathHelper.join(["./data", "test_folder", "test.txt"])}
 
     test "list_files/1 returns created file" do
       File.write(@file_attrs.filepath, "empty file", [:append])
       files = FileStorage.list_files(@file_attrs.folder)
+
       assert list_files_by_path(files, @file_attrs.filepath) != nil
     end
 
-
-    test "list_files/1 returns created file in properly parent" do
-      File.mkdir("./data/test_folder")
-      File.write("./data/test_folder/test.txt", "Som File info", [:append])
+    test "list_files/1 returns created file with properly parameters" do
+      File.write(@file_attrs.filepath, "empty file", [:append])
 
       files = FileStorage.list_files(@file_attrs.folder)
-      assert list_files_by_path(files, "./data/test_folder/test.txt") != nil
-
-      parent_files = FileStorage.list_files("./data/test_folder")
-      file = find_by_path(parent_files, "./data/test_folder/test.txt")
+      file = find_by_path(files, @file_attrs.filepath)
 
       assert file !== nil
       assert file.info.size != 0
       assert file.isFolder == false
       assert file.children == nil
+    end
 
-      File.rm!("./data/test_folder/test.txt")
-      File.rmdir!("./data/test_folder")
+
+    test "list_files/1 returns created file in properly parent" do
+      File.mkdir(@inner_file_attrs.folder)
+      File.write(@inner_file_attrs.filepath, "Some File info", [:append])
+
+      files = FileStorage.list_files(@file_attrs.folder)
+      assert list_files_by_path(files, @inner_file_attrs.filepath) != nil
+
+      parent_files = FileStorage.list_files(@inner_file_attrs.folder)
+      file = find_by_path(parent_files, @inner_file_attrs.filepath)
+      assert list_files_by_path(files, @file_attrs.filepath) != nil
+
+      File.rm!(@inner_file_attrs.filepath)
+      File.rmdir!(@inner_file_attrs.folder)
     end
 
     test "move_file/2 update file path properly" do
