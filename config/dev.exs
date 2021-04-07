6c6687d796201a05e76814379c409d53c8ed987d
Sapfir0 <sapfir999999@yandex.ru>
2021-04-07 21:14:10 +0300

    Перешел на react-scripts

--- config/dev.exs
+++ config/dev.exs
@@ -6,21 +6,7 @@ use Mix.Config
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
-config :elCloud, ElCloudWeb.Endpoint,
-  http: [port: 4000],
-  debug_errors: true,
-  code_reloader: true,
-  check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      "--env.NODE_ENV=dev",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+
 
 # Watch static and templates for browser reloading.
 config :elCloud, ElCloudWeb.Endpoint,
