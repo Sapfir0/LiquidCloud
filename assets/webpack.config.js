ea02ed71d60ee7a5ac96dc15d4c67a12f202c830
Sapfir0 <sapfir999999@yandex.ru>
2021-04-07 19:47:38 +0300

    Заменил awesome-ts на ts-loader

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -47,28 +47,6 @@ module.exports = (env) => {
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
         },
-        // optimization: {
-        //     runtimeChunk: 'single',
-        //     splitChunks: {
-        //         chunks: 'all',
-        //         maxInitialRequests: Infinity,
-        //         minSize: 0,
-        //         cacheGroups: {
-        //             vendor: {
-        //                 test: /[\\/]node_modules[\\/]/,
-        //                 name(module) {
-        //                     // получает имя, то есть node_modules/packageName/not/this/part.js
-        //                     // или node_modules/packageName
-        //                     const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-        //                     // имена npm-пакетов можно, не опасаясь проблем, использовать
-        //                     // в URL, но некоторые серверы не любят символы наподобие @
-        //                     return `npm.${packageName.replace('@', '')}`;
-        //                 },
-        //             },
-        //         },
-        //     },
-        // },
         module: {
             rules: [
                 {
